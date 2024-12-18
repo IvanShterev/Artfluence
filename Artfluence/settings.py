35005330d282347786ed11c215a359ceb914c7fb
Ivan Shterev <ivan.shterevv@gmail.com>
2024-12-18 14:28:26 +0200

    removed secret key from settings

--- Artfluence/settings.py
+++ Artfluence/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,8 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9hms1o21=o*as)*wm%*us%ja$^60$bzlvn*5#)p26o4))7%w-g'
+dotenv_path = os.path.join(BASE_DIR, 'venv/Lib/site-packages/dotenv/.env')
+load_dotenv(dotenv_path)
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
